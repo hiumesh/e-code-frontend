e6658d10378c3f8e3149c9b0432b8209d01a623c
hiumesh <daniumeshchandra.se@gmail.com>
2022-12-23 09:55:34 +0530

    Notification Context Added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 import "antd/dist/reset.css";
 import { AuthProvider, getUser, AuthTypes } from "../context/authProvider";
+import NotificationProvider from "../context/notificationProvider";
 import Layout from "../components/layout";
-import { NextPage, } from "next";
+import { NextPage } from "next";
 import App, { AppProps, AppContext } from "next/app";
 
 interface PropeTypes extends AppProps {
@@ -12,17 +13,21 @@ interface PropeTypes extends AppProps {
 const MyApp: NextPage<PropeTypes> = ({ Component, pageProps, auth }) => {
   return (
     <AuthProvider myAuth={auth}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <NotificationProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NotificationProvider>
     </AuthProvider>
   );
 };
 
 MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext as unknown as AppContext);
+  const appProps = await App.getInitialProps(
+    appContext as unknown as AppContext
+  );
   const auth = await getUser(appContext);
   return { ...appProps, auth } as PropeTypes;
 };
 
-export default MyApp
+export default MyApp;
